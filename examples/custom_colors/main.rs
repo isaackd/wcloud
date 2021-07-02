48eab57b3c884723cc181a186d548a455f7d368a
afrmtbl <afrmtbl@gmail.com>
2021-07-02 01:28:23 -0400

    Put examples in their own directories

--- examples/custom_colors/main.rs
+++ examples/custom_colors/main.rs
@@ -7,13 +7,13 @@ use palette::{Pixel, Srgb, Hsl, IntoColor};
 use image::Rgb;
 
 fn main() {
-    let script_path = "examples/a_new_hope.txt";
+    let script_path = "a_new_hope.txt";
     let script_text = fs::read_to_string(script_path)
         .expect("Unable to find a_new_hope.txt")
         .replace("HAN", "Han")
         .replace("LUKE'S", "Luke");
 
-    let exclude_words_path = "examples/python_stopwords";
+    let exclude_words_path = "python_stopwords";
     let exclude_words = fs::read_to_string(exclude_words_path)
         .expect(&format!("Unable to read exclude words file \'{}\'", exclude_words_path));
     let mut filter = exclude_words.split("\n")
@@ -26,7 +26,7 @@ fn main() {
         .with_max_words(0)
         .with_filter(filter);
 
-    let mut wordcloud = WordCloud::default()
+    let wordcloud = WordCloud::default()
         .with_tokenizer(tokenizer)
         .with_word_margin(10)
         .with_rng_seed(1);
@@ -52,5 +52,6 @@ fn main() {
 
     let wordcloud_image = wordcloud.generate_from_text_with_color_func(&script_text, mask, 1.0, color_func);
 
-    wordcloud_image.save("examples/a_new_hope.png");
+    wordcloud_image.save("examples/a_new_hope.png")
+        .expect("Unable to save image to examples/a_new_hope.png");
 }
