bed0827f865bcd46452fcaf0b929401d66e9473e
Isaac <afrmtbl@gmail.com>
2023-04-03 00:47:33 -0400

    Include files in examples binary

--- examples/custom_colors/main.rs
+++ examples/custom_colors/main.rs
@@ -1,15 +1,12 @@
-use std::fs;
 use std::collections::HashSet;
 use wcloud::{Tokenizer, WordCloud, WordCloudSize, Word, DEFAULT_EXCLUDE_WORDS_TEXT};
 use rand::Rng;
 use rand::rngs::StdRng;
 use palette::{Pixel, Srgb, Hsl, IntoColor};
-use image::Rgb;
+use image::{ImageFormat, Rgb};
 
 fn main() {
-    let script_path = "examples/custom_colors/a_new_hope.txt";
-    let script_text = fs::read_to_string(script_path)
-        .expect("Unable to find a_new_hope.txt")
+    let script_text = include_str!("a_new_hope.txt")
         .replace("HAN", "Han")
         .replace("LUKE'S", "Luke");
 
@@ -28,8 +25,11 @@ fn main() {
         .with_word_margin(10)
         .with_rng_seed(1);
 
-    let mask_path = "examples/custom_colors/stormtrooper_mask.png";
-    let mask_image = image::open(mask_path).unwrap().to_luma8();
+    let mask_buf = include_bytes!("stormtrooper_mask.png");
+    let mask_image = image::load_from_memory_with_format(mask_buf, ImageFormat::Png)
+        .expect("Unable to load mask from memory")
+        .to_luma8();
+
     let mask = WordCloudSize::FromMask(mask_image);
 
     let color_func = |_word: &Word, rng: &mut StdRng| {
@@ -46,6 +46,6 @@ fn main() {
 
     let wordcloud_image = wordcloud.generate_from_text_with_color_func(&script_text, mask, 1.0, color_func);
 
-    wordcloud_image.save("examples/custom_colors/a_new_hope.png")
-        .expect("Unable to save image to examples/a_new_hope.png");
+    wordcloud_image.save("a_new_hope.png")
+        .expect("Unable to save image a_new_hope.png");
 }
